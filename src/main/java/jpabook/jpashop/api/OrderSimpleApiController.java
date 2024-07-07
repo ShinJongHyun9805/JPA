3395d121ef7943003d48e091afec5f0f57284b59
ShinJongHyun9805 <75189110+ShinJongHyun9805@users.noreply.github.com>
2024-07-08 07:14:02 +0900

    simple-orders V2, V3 생성 (#5)
    * 상품 조회 API 추가
    
    * @JsonIgnore 주석 추가
    
    * 양방향 연관관계 주석처리
    
    * simple-orders V2, V3 생성
    
    ---------
    
    Co-authored-by: jhshin <jakie580@gmail.com>

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -22,11 +27,45 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
+    // 해당 API N + 1 문제 발생
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         return orderRepository.findAllByString(new OrderSearch());
     }
 
 
+    // 해당 API N + 1 문제 발생
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        return orderRepository.findAllByString(new OrderSearch())
+                .stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
 
+    // 위 N + 1 문제를 해결하기 위해 fetch Join 적용
+    // fetch => 한 방에 join 된 테이블 다 조회
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        return orderRepository.findAllWithMemberDelivery().stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    private class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
+    }
 }
