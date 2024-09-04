13d7021effcf6c6b67875b517208c574a1a43a13
ShinJongHyun9805 <75189110+ShinJongHyun9805@users.noreply.github.com>
2024-09-04 22:44:13 +0900

    AUDITING 추가 (#11)
    Co-authored-by: jhshin <jakie580@gmail.com>

---
+++ src/main/java/jpabook/jpashop/domain/entity/BaseEntity.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;       //
+                                    // -> @Bean AuditorAware<String> auditorProvider()의 값을 spring이 받아서 Set 해줌.
+    @LastModifiedBy                 //
+    private String lastModifiedBy;
+
+}
