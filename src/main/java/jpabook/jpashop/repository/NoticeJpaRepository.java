827ebb95deee40f928a8a1554b2c7f4c7ebfabee
jhshin <jakie580@gmail.com>
2024-10-07 23:45:54 +0900

    paing을 위한 Notice 추가

---
+++ src/main/java/jpabook/jpashop/repository/NoticeJpaRepository.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.entity.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeJpaRepository extends JpaRepository<Notice, Long> {
+
+
+
+
+
+
+}
